0081c0d7757d61bfca2a271f94a445dea2775b1e
Raphain <raphain1@hotmail.com>
2021-03-04 17:19:47 -0300

    First Commit

---
+++ EstruturaDoPrograma/EstruturaDoPrograma/Pilha.cs
@@ -0,0 +1,38 @@
+namespace EstruturaDoPrograma
+{
+    public class Pilha
+    {
+        Posicao primeiro;
+
+        public void Empilha (object item)
+        {
+            primeiro = new Posicao(primeiro, item);
+        }
+
+        public object Desempilha()
+        {
+            if (primeiro == null)
+            {
+                throw new System.Exception();
+            }
+
+            object resultado = primeiro.item;
+            primeiro = primeiro.proximo;
+            return resultado;
+        }
+
+        class Posicao
+        {
+            public Posicao proximo;
+            public object item;
+            public Posicao(Posicao proximo, object item)
+            {
+                this.proximo = proximo;
+                this.item = item;
+                
+            }
+
+
+        }
+    }
+}
